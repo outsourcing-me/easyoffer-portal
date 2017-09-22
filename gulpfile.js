07731f499739a3532057f1596485b7bf4204e674
luxueyan <luxueyan@kaitongamc.com>
2017-09-22 18:43:55 +0800

    update source url

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ var gulp = require('gulp')
 var ghPages = require('gulp-gh-pages')
 
 gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
+  return gulp.src('./docs/**/*')
     .pipe(ghPages())
 })
 
 var replace = require('gulp-replace')
 
 gulp.task('generate', function() {
-  gulp.src(['./dist/**/*.js', './dist/**/*.css', './dist/**/*.html'])
+  gulp.src(['./docs/**/*.js', './docs/**/*.css', './docs/**/*.html'])
     .pipe(replace('/_nuxt/', '_nuxt/'))
     .pipe(replace('/favicon.png', 'favicon.png'))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./docs'))
 })
